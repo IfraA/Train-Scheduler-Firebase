34443579e154c9bfce709148ce6455ffbbc4bd1c
IfraA <ifraa.aijaz@gmail.com>
2018-03-03 23:06:55 -0800

    layout

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+//Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAVkz8AQGonZnVS8LOOZevy6c4fuoJuA9E",
+  authDomain: "train-schedule-d0102.firebaseapp.com",
+  databaseURL: "https://train-schedule-d0102.firebaseio.com",
+  projectId: "train-schedule-d0102",
+  storageBucket: "",
+  messagingSenderId: "87853022855",
+};
+firebase.initializeApp(config);
